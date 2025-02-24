b2b1b419d325f28a38a1270ebdf34efed7bbb8a4
vu694273 <duyvu11092004@gmail.com>
2025-02-24 20:23:32 +0700

    [update] ex02_05 and ex03_05

--- lab-01/ex03/ex03_05.py
+++ lab-01/ex03/ex03_05.py
@@ -8,7 +8,7 @@ def dem_so_lan_xuat_hien(lst):
     return count_dict
 
 input_string = input("Nhập danh sách các từ, cách nhau bằng dấu cách: ")
-word_list = input_string.split()
+word_list = input_string.split(",")
 
 so_lan_xuat_hien = dem_so_lan_xuat_hien(word_list)
 print("Số lần xuất hiện của mỗi từ trong danh sách là: ", so_lan_xuat_hien)
